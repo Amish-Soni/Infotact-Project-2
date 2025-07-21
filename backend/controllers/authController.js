14b734f6ed954bdaf039ab4e40501f6cd5c45c8a
Amish Soni <soni2003amish@gmail.com>
2025-07-21 17:07:04 +0530

    Backend Fixes in Production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -33,8 +33,8 @@ export const registerController = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false, // change to true in production with HTTPS
-        sameSite: "Lax",
+        secure: true, // change to true in production with HTTPS
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .status(201)
@@ -69,8 +69,8 @@ export const loginController = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "Lax",
+        secure: true,
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json({
